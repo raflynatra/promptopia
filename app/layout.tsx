fb853c4445e98ad14b8b42bce81795e074089753
Rafly Pradana Putra <rafly.pradana@inticorp.tech>
2024-04-30 14:32:57 +0700

    Initiate project setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "@/styles/global.css";
+import { FC, ReactNode } from "react";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
