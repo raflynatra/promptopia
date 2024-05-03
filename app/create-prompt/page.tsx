c8a089c0a060b9a7379161c2bb7b0c80f3c1cfd7
raflynatra <raflypradana31@gmail.com>
2024-05-03 17:39:10 +0700

    feat: add create prompt feature

---
+++ app/create-prompt/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@/components/Form";
+
+export type TPost = {
+  prompt: string;
+  tag: string;
+};
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState<TPost>({
+    prompt: "",
+    tag: "",
+  });
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const createPrompt = async (e: FormEvent) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
